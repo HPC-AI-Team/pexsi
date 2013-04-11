ffad0b101f9f5acff8019ead84a756532b81ac5d
Lin Lin <linlin@lbl.gov>
2013-04-10 23:34:10 -0700

    New interface for DNA matrix (f_ppexsi)

--- examples/f_ppexsi.f90
+++ examples/f_ppexsi.f90
@@ -1,8 +1,8 @@
-!> @file ex20.f90
-!> @brief Test the FORTRAN interface for PEXSI.
+!> @file ex22.f90
+!> @brief Test the new FORTRAN interface for PPEXSI.
 !> @author Lin Lin
-!> @date 2013-01-31
-program ex20
+!> @date 2013-04-10
+program ex22
 implicit none
 include 'mpif.h'
 
@@ -10,19 +10,24 @@ integer :: nrows, nnz, nnzLocal, numColLocal
 integer, allocatable, dimension(:) ::  colptrLocal, rowindLocal
 double precision, allocatable, dimension(:) :: &
 	HnzvalLocal, SnzvalLocal, DMnzvalLocal, EDMnzvalLocal, &
-	FDMnzvalLocal, muList, numElectronList, numElectronDrvList
+	FDMnzvalLocal, muList, numElectronList, numElectronDrvList,&
+	shiftList, inertiaList
 integer :: numPole
-double precision :: temperature, numElectronExact, numElectron,&
+double precision :: K2au, temperature, numElectronExact, numElectron,&
 	gap, deltaE
-double precision :: mu, muZeroT, muMin, muMax
-integer:: muMaxIter, muIter
+double precision ::   muMin0, muMax0, muInertia, muMinInertia, muMaxInertia,&
+	muLowerEdge, muUpperEdge, muPEXSI, muMinPEXSI, muMaxPEXSI
+
+integer:: inertiaMaxIter, inertiaIter, muMaxIter, muIter
 integer:: ordering
 integer:: isInertiaCount
-double precision :: poleTolerance, numElectronTolerance
+double precision :: poleTolerance, PEXSINumElectronTolerance, &
+	inertiaNumElectronTolerance
 integer:: npPerPole, nprow, npcol
 integer :: mpirank, mpisize, ierr
 double precision:: timeSta, timeEnd
 character*32 :: Hfile, Sfile
+integer:: isSIdentity
 ! Communicator for reading the matrix, with size npPerPole
 integer:: readComm
 integer:: isProcRead
@@ -34,36 +39,44 @@ call mpi_comm_size( MPI_COMM_WORLD, mpisize, ierr )
 
 
 ! Data is for the DNA matrix.
-temperature      = 300.0d0
+K2au             = 3.1668152d-6
+! Temperature should be in the same unit as the H matrix. Here it is Hartree.
+temperature      = 300.0d0 * K2au
+
 numElectronExact = 2442.0d0
 numPole          = 64
 gap              = 0.0d0
 ! deltaE is in theory the spectrum width, but in practice can be much smaller
 ! than | E_max - mu |.  It is found that deltaE that is slightly bigger
 ! than  | E_min - mu | is usually good enough.
-deltaE           = 3.0d0
-! Initial guess of chemical potential, also updated after pexsi.
-mu               = -0.60d0
-! Lower/Upper bound for the chemical potential.
-muMin            = -1.0d0
-muMax            =  0.0d0
-! muMaxIter should be 1 or 2 later when combined with SCF.
-muMaxIter        = 1
+deltaE           = 30.0d0
+! Initial searching interval for the chemical potential for inertia count.
+muMin0           = -1.0d0
+muMax0           =  0.0d0
+! Maximum number of iterations for computing the inertia
+inertiaMaxIter   = 3
+! Maximum number of iterations for PEXSI iteration
+muMaxIter        = 3
 ! Do not compute a pole if the corresponding weight is < poleTolerance.
 poleTolerance    = 1d-8
+! Stop inertia count if Ne(muMax) - Ne(muMin) < inertiaNumElectronTolerance
+inertiaNumElectronTolerance = 100
 ! Stop mu-iteration if numElectronTolerance is < numElectronTolerance.
-numElectronTolerance = 1d-2
+PEXSINumElectronTolerance = 1d-2
 ! Number of processors used for each pole. At the moment use mpisize.
 ! Later can be changed to 
 npPerPole        = 16
 Hfile            = "H.matrix"
+! Empty Sfile means the overlap matrix is identity
 Sfile            = "S.matrix"
+! Whether S is an identity matrix
+isSIdentity      = 0
+
 ! Ordering 
 !   0   : PARMETIS
 !   1   : METIS_AT_PLUS_A
 !   2   : MMD_AT_PLUS_A
 ordering         = 1
-isInertiaCount   = 1
 
 
 ! Read and compute the size/local size of the arrays 
@@ -82,6 +95,10 @@ call mpi_comm_split( MPI_COMM_WORLD, isProcRead, mpirank, readComm, ierr )
 allocate( muList( muMaxIter ) )
 allocate( numElectronList( muMaxIter ) )
 allocate( numElectronDrvList( muMaxIter ) )
+
+allocate( shiftList( numPole ) )
+allocate( inertiaList( numPole ) )
+
 
 if( isProcRead == 1 ) then
 	write(*,*) "Proc ", mpirank, " is reading file..."
@@ -123,16 +140,18 @@ if( isProcRead == 1 ) then
 		HnzvalLocal,&
 		readComm )
 
-	call f_read_distsparsematrix_formatted (&
-		trim(Sfile)//char(0),&
-		nrows,&
-		nnz,&
-		nnzLocal,&
-		numColLocal,&
-		colptrLocal,&
-		rowindLocal,&
-		SnzvalLocal,&
-	  readComm )	
+	if( isSIdentity == 0 ) then
+		call f_read_distsparsematrix_formatted (&
+			trim(Sfile)//char(0),&
+			nrows,&
+			nnz,&
+			nnzLocal,&
+			numColLocal,&
+			colptrLocal,&
+			rowindLocal,&
+			SnzvalLocal,&
+			readComm )	
+	endif
 
 	timeEnd = mpi_wtime()
 
@@ -147,7 +166,38 @@ if( mpirank == 0 ) then
 endif
 
 
-call f_ppexsi_interface( &
+call f_ppexsi_inertiacount_interface(&
+	nrows,&
+	nnz,&
+	nnzLocal,&
+	numColLocal,&
+	colptrLocal,&
+	rowindLocal,&
+	HnzvalLocal,&
+	isSIdentity,&
+	SnzvalLocal,&
+	temperature,&
+	numElectronExact,&
+	muMin0,&
+	muMax0,&
+	numPole,&
+	inertiaMaxIter,&
+	inertiaNumElectronTolerance,&
+	ordering,&
+	npPerPole,&
+	MPI_COMM_WORLD,&
+	muMinInertia,&
+	muMaxInertia,&
+	muLowerEdge,&
+	muUpperEdge,&
+	inertiaIter,&
+	shiftList,&
+	inertiaList)
+
+muInertia    = (muLowerEdge + muUpperEdge) / 2d0
+
+
+call f_ppexsi_solve_interface(&
 	nrows,&
 	nnz,&
 	nnzLocal,&
@@ -155,54 +205,60 @@ call f_ppexsi_interface( &
 	colptrLocal,&
   rowindLocal,&
 	HnzvalLocal,&
+	isSIdentity,&
 	SnzvalLocal,&
+	temperature,&
+	numElectronExact,&
+	muInertia,&
+	muMinInertia,&
+	muMaxInertia,&
+	gap,&
+	deltaE,&
+	numPole,&
+	muMaxIter,&
+	PEXSINumElectronTolerance,&
+	poleTolerance,&
+	ordering,&
+	npPerPole,&
+	MPI_COMM_WORLD,&
 	DMnzvalLocal,&
 	EDMnzvalLocal,&
 	FDMnzvalLocal,&
-	numPole,&
-	temperature,&
-	numElectronExact,&
+	muPEXSI,&
 	numElectron,&
-	gap,&
-	deltaE,&
-	mu,&
-	muMin,&
-  muMax,&
-	muMaxIter,&
-	ordering,&
-	isInertiaCount,&
+	muMinPEXSI,&
+	muMaxPEXSI,&
 	muIter,&
 	muList,&
 	numElectronList,&
-	numElectronDrvList,&
-	muZeroT,&
-	poleTolerance,&
-	numElectronTolerance,&
-	MPI_COMM_WORLD,&
-	npPerPole )
+	numElectronDrvList)
 
 if( mpirank == 0 ) then
-	write(*, *) "mu          = ", mu
-	write(*, *) "guess of mu(T=0) = ", muZeroT
-	write(*, *) "numElectron = ", numElectron
-	write(*, *) "number of mu iterations = ", muIter
-	write(*, *) "mu history"
-	do i = 1, muIter
-		write(*,*) muList(i)
-	enddo
-	write(*, *) "numElectron history"
-	do i = 1, muIter
-		write(*,*) numElectronList(i)
-	enddo
-	write(*, *) "dN/dMu history"
-	do i = 1, muIter
-		write(*,*) numElectronDrvList(i)
-	enddo
+	write(*, *) "After inertia count,"
+	write(*, *) "muMinInertia  = ", muMinInertia
+	write(*, *) "muMaxInertia  = ", muMaxInertia
+	write(*, *) "muLowerEdge   = ", muLowerEdge
+	write(*, *) "muUpperEdge   = ", muUpperEdge
+	write(*, *) "inertiaIter   = ", inertiaIter
+	! write(*, *) "shift ,           inertia count"
+	! do i = 1, numPole 
+		! write(*,*) shiftList(i), inertiaList(i)
+	! enddo
+	write(*, *) 
+	write(*, *) "After PEXSI,"
+	write(*, *) "muPEXSI       = ", muPEXSI
+	write(*, *) "numElectron   = ", numElectron
+	write(*, *) "muMinPEXSI    = ", muMinPEXSI
+	write(*, *) "muMaxPEXSI    = ", muMaxPEXSI
+	write(*, *) "muIter        = ", muIter
 endif
 
 deallocate( muList )
 deallocate( numElectronList )
 deallocate( numElectronDrvList )
+
+deallocate( shiftList )
+deallocate( inertiaList )
 
 if( isProcRead == 1 ) then
 	deallocate( colptrLocal )
@@ -217,5 +273,5 @@ endif
 
 call mpi_finalize( ierr )
 
-end program ex20
+end program ex22
 
