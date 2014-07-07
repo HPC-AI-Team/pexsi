96456433ef5929e35f0bf4cbc0bcba0a36100f49
mathias jacquelin <mjacquelin@lbl.gov>
2014-07-07 07:56:49 -0700

    trying to use ngchol in conjunction with PEXSI, almost done

--- tests/ex32.cpp
+++ tests/ex32.cpp
@@ -5,64 +5,89 @@
 
 #include <mpi.h>
 
+#include <complex>
+
 // Libraries from NGCHOL BEGIN
-#include <time.h>
-#include <random>
-#include <omp.h>
+//#include <time.h>
+//#include <random>
+//#include <omp.h>
 
-#include  "Environment.hpp"
-#include  "DistSparseMatrix.hpp"
-#include  "NumVec.hpp"
-#include  "NumMat.hpp"
-#include  "utility.hpp"
-#include  "ETree.hpp"
-#include  "Mapping.hpp"
+#include "ngchol.hpp"
 
-#include  "blas.hpp"
-#include  "lapack.hpp"
-//#include  "NZBlock.hpp"
-#include  "SuperNode.hpp"
-#include  "SupernodalMatrix.hpp"
-
-//#include <async.h>
-#include  "LogFile.hpp"
-
-
-extern "C" {
-#include <bebop/util/config.h>
-#include <bebop/smc/sparse_matrix.h>
-#include <bebop/smc/csr_matrix.h>
-#include <bebop/smc/csc_matrix.h>
-#include <bebop/smc/sparse_matrix_ops.h>
-
-#include <bebop/util/get_options.h>
-#include <bebop/util/init.h>
-#include <bebop/util/malloc.h>
-#include <bebop/util/timer.h>
-#include <bebop/util/util.h>
-}
-// Libraries from NGCHOL END
+//#include  "Environment.hpp"
+//#include  "DistSparseMatrix.hpp"
+////#include  "NumVec.hpp"
+////#include  "NumMat.hpp"
+//#include  "utility.hpp"
+////#include  "ETree.hpp"
+//#include  "Mapping.hpp"
+//
+////#include  "blas.hpp"
+////#include  "lapack.hpp"
+//////#include  "NZBlock.hpp"
+//
+//#include  "SuperNode.hpp"
+//#include  "SupernodalMatrix.hpp"
+//
+//#include  "LogFile.hpp"
 
 
 
+//using namespace LIBCHOLESKY;
 using namespace PEXSI;
 using namespace std;
 
 void Usage(){
   std::cout 
-		<< "Test to use the NGCHOL library in PEXSI." 
+    << "Test to use the NGCHOL library in PEXSI." 
     << std::endl << std::endl;
 }
 
-
+//typedef std::complex<double> SCALAR;
+typedef double SCALAR;
 
 int main(int argc, char **argv) 
 {
+  int mpisize;
+  int mpirank;
+
+  MPI_Init(&argc,&argv);
+
+  //Create a communicator with npcol*nprow processors
+  MPI_Comm world_comm;
+  MPI_Comm_dup(MPI_COMM_WORLD, &world_comm);
+
+  MPI_Comm_size(world_comm, &mpisize);
+  MPI_Comm_rank(world_comm, &mpirank);
+
+  stringstream  ss;
+  ss << "logTest" << mpirank;
+  statusOFS.open( ss.str().c_str() );
 
   if( mpirank == 0 ) {
     Usage();
   }
 
+  LIBCHOLESKY::logfileptr = new LIBCHOLESKY::LogFile(mpirank);
+  LIBCHOLESKY::logfileptr->OFS()<<"********* LOGFILE OF P"<<mpirank<<" *********"<<endl;
+  LIBCHOLESKY::logfileptr->OFS()<<"**********************************"<<endl;
+
+
+
+
+  //LIBCHOLESKY::Modwrap2D * mapping = new LIBCHOLESKY::Modwrap2D(mpisize, mpisize, mpisize, 1);
+  LIBCHOLESKY::Modwrap2D * mapping = new LIBCHOLESKY::Modwrap2D(mpisize, sqrt(mpisize), mpisize, 1);
+  LIBCHOLESKY::DistSparseMatrix<SCALAR > HMat(world_comm);
+  LIBCHOLESKY::ParaReadDistSparseMatrix( argv[1], HMat, world_comm ); 
+
+  LIBCHOLESKY::SupernodalMatrix<SCALAR> SMat(HMat,-1,*mapping,0,0,world_comm);
+  statusOFS<<"Hello"<<endl;
+  for(Int i = 0; i<SMat.SupernodeCnt();++i){
+    LIBCHOLESKY::SuperNode<SCALAR> curSnode = SMat.GetLocalSupernode(i);
+    statusOFS<<curSnode<<endl;
+  }
+
+  delete mapping;
 
   try{
   }
@@ -74,6 +99,10 @@ int main(int argc, char **argv)
     DumpCallStack();
 #endif
   }
+
+  delete LIBCHOLESKY::logfileptr;
+  statusOFS.close();
+
 
   MPI_Finalize();
 
---
+++ tests/ngchol.hpp
@@ -0,0 +1,17 @@
+#ifndef _NGCHOL_HEADER_
+#define _NGCHOL_HEADER_
+
+#include  "Environment.hpp"
+#include  "utility.hpp"
+#include  "DistSparseMatrix.hpp"
+//#include  "NumVec.hpp"
+//#include  "NumMat.hpp"
+
+#include  "Mapping.hpp"
+#include  "SuperNode.hpp"
+#include  "SupernodalMatrix.hpp"
+
+#include  "LogFile.hpp"
+
+
+#endif // _NGCHOL_HEADER_
