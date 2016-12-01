89ffc9cedac637b825d3dc4f55e0ea280c352f69
linlin <linlin@math.berkeley.edu>
2016-11-30 21:54:13 -0800

    Correct the inconsistent Dotu behavior in CalculateFermiOperatorComplex
    o NOTE: Depending on the treatment of matrix transpose, in the future
    this needs to be checked again with the new asymmetric code!

--- utilities/ReadComplexSIESTASparseMatrix.m
+++ utilities/ReadComplexSIESTASparseMatrix.m
@@ -8,6 +8,8 @@ fid = fopen(filename,'r');
 N = fscanf(fid, '%g', 1);
 N = fscanf(fid, '%g', 1);
 Annz = fscanf(fid, '%g', 1);
+dummy = fscanf(fid, '%g', 1);
+
 colptr = fscanf(fid, '%g', N+1);
 rowind = fscanf(fid, '%g', Annz);
 Aval   = fscanf(fid, '%g', 2*Annz);
