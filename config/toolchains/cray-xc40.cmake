0fd936a1901db53599fa15fafc3faa9aed5543cc
David Williams-Young <dbwy@lbl.gov>
2021-02-24 12:10:51 -0800

    Update Cray toolchain

--- config/toolchains/cray-xc40.cmake
+++ config/toolchains/cray-xc40.cmake
@@ -3,7 +3,7 @@ set( CMAKE_C_COMPILER       cc   )
 set( CMAKE_CXX_COMPILER     CC   )
 set( CMAKE_Fortran_COMPILER ftn  )
 
-set( SuperLU_DIST_PREFIX    "/path/to/superlu/install"  )
-set( ParMETIS_PREFIX        "/path/to/parmetis/install" )
+set( SuperLU_DIST_PREFIX    "/global/project/projectdirs/m1027/shared_libraries_cori_intel/SuperLU_DIST_6.4.0/install"  )
+set( ParMETIS_PREFIX        "/project/projectdirs/m1027/shared_libraries_cori_intel/parmetis_v0.4.3/install" )
 
 # BLAS and LAPACK are auto deduced
